1a8a66c5cbd91b8dd2437a10165b244ce7da6ecd
ankitgupta <ankitgupta@topsinfosolutions.com>
2023-09-30 15:42:08 +0530

    added blank tasks list message

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,12 +4,16 @@ import TaskDetails from "./TaskDetails";
 
 const TaskList: React.FC<{ tasksList: string[] }> = ({ tasksList }) => (
   <>
-    {tasksList.map((taskId: string, index: number) => {
-      const task = taskData.find((task) => task.id === taskId);
-      return task ? (
-        <TaskDetails task={task} index={index} key={task.id} />
-      ) : null;
-    })}
+    {tasksList.length !== 0 ? (
+      tasksList.map((taskId: string, index: number) => {
+        const task = taskData.find((task) => task.id === taskId);
+        return task ? (
+          <TaskDetails task={task} index={index} key={task.id} />
+        ) : null;
+      })
+    ) : (
+      <p className="text-center text-danger">No tasks for today !!</p>
+    )}
   </>
 );
 
